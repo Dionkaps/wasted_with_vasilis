1790f7f6d2d78a459a54b9c99e3514278a1bd118
Dionisis Konstantinos Kapsimalis <vpddkf@gmail.com>
2023-03-28 11:59:51 +0300

    simple layout plus firebase conn

--- wwv/lib/main.dart
+++ wwv/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -14,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
@@ -32,7 +36,109 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Wasted With Vasilis'),
+        title: Text('Wasted On Vasilis'),
+      ),
+      body: SizedBox(
+        height: 400,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 60),
+              child: const Expanded(
+                child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Total time wasted",
+                      style:
+                          TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                    )),
+              ),
+            ),
+            const Expanded(
+              child: Align(alignment: Alignment.center, child: Text("XXXX")),
+            ),
+            const Expanded(
+              child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "The equivalent of",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  )),
+            ),
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: Column(
+                  children: const [
+                    Expanded(
+                      child: Align(
+                          alignment: Alignment.center, child: Text("XXXX")),
+                    ),
+                    Expanded(
+                      child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            "books read",
+                            style: TextStyle(
+                                fontSize: 17, fontWeight: FontWeight.bold),
+                          )),
+                    ),
+                  ],
+                )),
+                Expanded(
+                    child: Column(
+                  children: const [
+                    Expanded(
+                      child: Align(
+                          alignment: Alignment.center, child: Text("XXXX")),
+                    ),
+                    Expanded(
+                      child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            "movies watched",
+                            style: TextStyle(
+                                fontSize: 17, fontWeight: FontWeight.bold),
+                          )),
+                    ),
+                  ],
+                )),
+                Expanded(
+                    child: Column(
+                  children: const [
+                    Expanded(
+                      child: Align(
+                          alignment: Alignment.center, child: Text("XXXX")),
+                    ),
+                    Expanded(
+                      child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            "km walked",
+                            style: TextStyle(
+                                fontSize: 17, fontWeight: FontWeight.bold),
+                          )),
+                    ),
+                  ],
+                )),
+              ],
+            )),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          final docTime = FirebaseFirestore.instance
+              .collection('totalTime')
+              .doc('wastedTime');
+
+              docTime.update({
+                'time': 2,
+              });
+        },
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.navigation),
       ),
     );
   }
